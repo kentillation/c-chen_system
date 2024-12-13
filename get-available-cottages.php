82923dedd9d1eea7f9cb7e1735341de2fb1f3dfa
Kent Anthony Engbino <kentanthony070495@gmail.com>
2024-12-13 09:16:32 +0800

    Updated

--- get-available-cottages.php
+++ get-available-cottages.php
@@ -10,6 +10,13 @@ if (isset($_POST['date_check_in'])) {
     $stmt->execute();
     $result = $stmt->get_result();
     $row = $result->fetch_assoc();
-    $availableCottage = $row['booked_cottage'] ?? 0 . " available cottage(s)";
-    echo json_encode(['available_cottages' => $availableCottage]);
+    $availCottage = $row['booked_cottage'] ?? 0;
+    $totalCottageStmt = $conn->prepare(" SELECT COUNT(*) as total_cottages FROM tbl_cottages WHERE cottage_id = ? ");
+    $totalCottageStmt->bind_param('i', $cottage_id);
+    $totalCottageStmt->execute();
+    $totalCottageResult = $totalCottageStmt->get_result();
+    $totalCottageRow = $totalCottageResult->fetch_assoc();
+    $totalCottage = $totalCottageRow['total_cottages'] ?? 0;
+    $availableCottages = $totalCottage - $availCottage;
+    echo json_encode(['available_cottages' => $availableCottages]);
 }
